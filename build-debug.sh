00b0aee30e1b65383906f22e6b1ec0d8be9a3070
kittech <kittech7@gmail.com>
2025-02-22 21:28:55 +0100

    adding build scripts

---
+++ build-debug.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+error () {
+  echo $1
+  exit 1
+}
+
+if ! cd ./build-debug/
+then
+   mkdir ./build-debug/
+   cd ./build-debug/ || error "unable to create build director"
+fi
+
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake --build . --config Debug
+cd ..
+
